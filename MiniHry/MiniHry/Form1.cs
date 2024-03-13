924ad9704e0be21f906a245ce90c8d858b64de6b
Adam Justin <92076162+KakyoinRedhead@users.noreply.github.com>
2024-03-13 20:45:53 +0100

    minihry

---
+++ MiniHry/MiniHry/Form1.cs
@@ -0,0 +1,27 @@
+namespace MiniHry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Nastaveni nastaveni = new Nastaveni();
+            nastaveni.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
